eb8b03803dad295d03f6c48a0ab5d61cfced5c6e
jpendli-bdi <jpendli@entergy.com>
2024-11-11 13:17:39 -0500

    Update to include Notes

--- src/app.py
+++ src/app.py
@@ -1,27 +1,45 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
 import pickle
-from flask import Flask, jsonify
-
+from flask_cors import CORS
+ 
 app = Flask(__name__)
-
-@app.route('/fetchComponent/<parameter>')
-def loadData(parameter):
-    # for reading also binary mode is important
-    dbfile = open('/opt/vector/ADEL1_metadata.pkl', 'rb')    
-    db = pickle.load(dbfile)
-    #for keys in db:
-       # print(keys, '=>', db[keys])
-    for item in db.values():
-        #print('The data is : ', item['component_number'])
-        if(item['component_number']==parameter):
-            return jsonify(item['structured_data'])
-   # print("Values:", list(db.values()))
-    dbfile.close()
-   
-
-
-@app.route('/hello')
-def hello_world():
-    return jsonify({'message': 'Hello, World!'})
-
+#needs to use environment variable
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+api = Api(app)
+ 
+class ComponentData(Resource):
+    def get(self, parameter):
+        try:
+            with open('./data/ADEL_metadata.pkl', 'rb') as dbfile,open('./data/ADEL1_metadata.pkl', 'rb') as db1file,open('./data/ADEL2_metadata.pkl', 'rb') as db2file:
+                db = pickle.load(dbfile)    	   
+                db1 = pickle.load(db1file)    	    
+                db2 = pickle.load(db2file)
+            for item in db.values():
+            	if(item['valve_id']==parameter):            
+                   data1=item['structured_data']
+            for item in db1.values():
+                if(item['component_number']==parameter):            
+                   data2=item['structured_data']
+            for item in db2.values():
+                if(item['Item_Number']==data2['Alignment Procedure']):
+                    data3=item['Notes']
+    	       
+            merged_data = {**data1, **data2}
+            merged_data['Notes']=data3;
+            return jsonify(merged_data)
+        except Exception as e:
+            return {'error': str(e)}, 500
+	  
+    # You can add other HTTP methods like POST, PUT, DELETE as needed
+ 
+class HelloWorld(Resource):
+    def get(self):
+        return {'message': 'Hello, World!'}, 200
+ 
+api.add_resource(ComponentData, '/fetchComponent/<string:parameter>')
+api.add_resource(HelloWorld, '/hello')
+ 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host="0.0.0.0", debug=True)
+ 
